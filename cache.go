ace555c0ee882ee11e94e8f6c501baeffbaf515f
Denis Ravinski <delgoden@gmail.com>
2022-05-26 15:13:21 +0300

    implemented specified in README.md functional

--- cache.go
+++ cache.go
@@ -1,23 +1,52 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
+	sync.RWMutex
+	data map[string]string
 }
 
-func NewCache() Cache {
-	return Cache{}
+func NewCache() *Cache {
+	return &Cache{data: make(map[string]string)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	c.RLock()
+	if value, ok := c.data[key]; ok {
+		return value, ok
+	}
+	c.RUnlock()
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.Lock()
+	c.data[key] = value
+	c.Unlock()
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.data))
+	c.RLock()
+	for key := range c.data {
+		keys = append(keys, key)
+	}
+	c.RUnlock()
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.Lock()
+	c.data[key] = value
+	c.Unlock()
+	go func(deadline time.Time, key string) {
+		time.Sleep(deadline.Sub(time.Now()))
+		c.Lock()
+		delete(c.data, key)
+		c.Unlock()
+	}(deadline, key)
 }
